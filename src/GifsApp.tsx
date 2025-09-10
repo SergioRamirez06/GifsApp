fb57affb6036634ee9be76861206b1c6d33953d9
sergioramirez06 <sergio200206mayo@gmail.com>
2025-09-10 14:03:28 -0500

    feat: update GifsApp.tsx

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react';
-import { mockGifs } from './data/gifs.mock';
 import { SearchBar } from "./shared/components/SearchBar"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { GifsList } from './gifs/components/GifsList';
-import { getGiphyByQuery } from './gifs/actions/get-gifs-by-query.action';
-import { Gif } from './gifs/interfaces/gif-interface';
-
+import { useGifs } from './gifs/hook/useGifs';
 
 const terms: string[] = []
 
 export const GifsApp = () => {
-    const [previousTerms, setPreviousTerms] = useState( terms );
-    const [gifs, setGifs] = useState<Gif[]>([]);
 
-    const handletermClick = ( term: string ) => {
-        console.log({term})
-    }
-
-    const handleSearch = async( query: string ) => {
-        query.trim().toLowerCase()
-        if( query.length === 0 ) return;
-        if( previousTerms.includes(query) ) return;
-                
-        setPreviousTerms( [ query, ...previousTerms].slice(0,7) )
-
-        const gifs = await getGiphyByQuery(query);
-        setGifs(gifs)
-    }
-
+    const { gifs, handleSearch, handletermClick, previousTerms } = useGifs(terms)
+    
   return (
     <>
         <CustomHeader title="Buscador de Gifs" subtitle="Descubre y comparte Gifs"/>
